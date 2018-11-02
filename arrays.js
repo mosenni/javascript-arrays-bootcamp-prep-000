4af0e9fba1b8285afda04b8ca771b74c856e79ce
Mike Osenni <mosenni@fandm.edu>
2018-11-02 12:56:51 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+ return array.unshift()
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+return ["foo", ...array]
+}
+
+function accessElementInArray(array, index){
+  return array[2];
+}
+
+function addElementToEndOfArray(array, element){
+  return array.push()
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+ return array.push()
+}
